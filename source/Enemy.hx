5552661d1b4b651af2989a28a7235277ffa02b68
unknown <kevinjaems@gmail.com>
2014-11-12 14:03:10 -0500

    Added Enemy Class

---
+++ source/Enemy.hx
@@ -0,0 +1,18 @@
+
+package;
+
+import flixel.FlxSprite;
+import flixel.FlxG;
+import flixel.FlxObject;
+import flixel.util.FlxColor;
+import flixel.util.FlxAngle;
+
+class Enemy extends FlxSprite
+{
+	public function new(X:Float=0, Y:Float=0){
+		super(X,Y);
+		makeGraphic(16,16, FlxColor.WHITE);
+		immovable = true;
+	}
+	
+}
